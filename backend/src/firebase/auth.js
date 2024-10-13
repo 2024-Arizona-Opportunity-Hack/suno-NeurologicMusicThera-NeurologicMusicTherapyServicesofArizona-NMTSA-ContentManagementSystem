a8e9738201eeb0df7f18844cac476a6bf71d5c02
Tanmayi Ghadge <tanmayighadge@gmail.com>
2024-10-12 17:02:36 -0700

    firebase

--- backend/src/firebase/auth.js
+++ backend/src/firebase/auth.js
@@ -1,4 +1,4 @@
-import {auth, FIRESTORE_DB} from "./firebase";
+import { auth, FIRESTORE_DB } from "./firebase";
 import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
@@ -7,23 +7,35 @@ import {
   updatePassword,
   signInWithPopup,
   GoogleAuthProvider,
-  
 } from "firebase/auth";
 import { collection, deleteDoc, doc, getDoc, getDocs, setDoc } from "firebase/firestore";
-export const doCreateUserWithEmailAndPassword = async (email, password) => {
+export const doCreateUserWithEmailAndPassword = async (email, password, role) => {
   const users = await getDocs(collection(FIRESTORE_DB, "users"));
   //if its a first user, set it as admin
   let isAdmin = false;
   if (users.empty) {
     isAdmin = true;
   }
+//   if(!isAdmin  && inviteCode ===""){
+//  alert("Please enter a valid invite code");
+//  return false;
+//   }
 
+  //check if the invite code is valid
+//   if(!isAdmin){
+//   const inviteCodeDoc = await getDoc(doc(FIRESTORE_DB, "codes", inviteCode));
+//   if (!inviteCodeDoc.exists()) {
+//     alert("Invalid invite code");
+//     return false;
+//   }
+// }
   await createUserWithEmailAndPassword(auth, email, password);
 
+  // await deleteDoc(doc(FIRESTORE_DB, "codes", inviteCode));
   await setDoc(doc(FIRESTORE_DB, "users", email), {
     email: email,
     password: password,
-    roles: [isAdmin ? "admin" : "student"],
+    roles: [isAdmin ? "admin" : role],
   });
   return true;
 };
